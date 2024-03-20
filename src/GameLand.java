c6845f9aa6a7e4f144ba3aef55667ffad622c2fd
naomiyu_24 <naomi_yu24@milton.edu>
2024-03-20 12:33:15 -0400

    Screensaver project

---
+++ src/GameLand.java
@@ -0,0 +1,169 @@
+//Game Example
+//Lockwood Version 2023-24
+// Learning goals:
+// make an object class to go with this main class
+// the object class should have a printInfo()
+//input picture for background
+//input picture for object and paint the object on the screen at a given point
+//create move method for the object, and use it
+// create a wrapping move method and a bouncing move method
+//create a second object class
+//give objects rectangles
+//start interactions/collisions
+
+//*******************************************************************************
+//Import Section
+//Add Java libraries needed for the game
+//import java.awt.Canvas;
+
+//Graphics Libraries
+
+
+//*
+// Platform class
+// Bird class
+// Do eggs later
+// Bird move method & hero move method
+// Worry about the flying through platforms later
+// Start with bird movement and then hero movement
+// Gravity (accel/deccel) *//
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+//***MAKING OBJECTS***
+//(1) declare
+//(2) construct
+//(3) use
+
+//*******************************************************************************
+// Class Definition Section
+
+public class GameLand implements Runnable {
+
+    //Variable Declaration Section
+    //Declare the variables used in the program
+    //You can set their initial values here if you want
+
+    public Image background;
+    //Sets the width and height of the program window
+    final int WIDTH = 1000;
+    final int HEIGHT = 700;
+
+    //Declare the variables needed for the graphics
+    public JFrame frame;
+    public Canvas canvas;
+    public JPanel panel;
+
+    public BufferStrategy bufferStrategy;
+
+    //Declare the objects used in the program below
+    //(1)***DECLARING OBJECTS/IMAGES***
+
+    public Hero[] birds;
+
+    // Main method definition: PSVM
+    // This is the code that runs first and automatically
+    //***PSVM***
+    public static void main(String[] args) {
+        GameLand ex = new GameLand();   //creates a new instance of the game and tells GameLand() method to run
+        new Thread(ex).start();       //creates a thread & starts up the code in the run( ) method
+    }
+
+    // Constructor Method
+    // This has no return type and has the same name as the class
+    // This section is the setup portion of the program
+    // Initialize your variables and construct your program objects here.
+    //***CONSTRUCTOR***
+    public GameLand() {
+        setUpGraphics(); //this calls the setUpGraphics() method
+        //(2)***CONSTRUCTING OBJECTS***
+
+    }// GameLand()
+
+//*******************************************************************************
+//User Method Section
+//
+// put your code to do things here.
+
+    // main thread
+    // this is the code that plays the game after you set things up
+    public void run() {
+        //for the moment we will loop things forever using a while loop
+        while (true) {
+            moveThings();  //move all the game objects
+            render();  // paint the graphics
+            pause(20); // sleep for 20 ms
+        }
+    }
+
+    //paints things on the screen using bufferStrategy
+    private void render() {
+        Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+        g.clearRect(0, 0, WIDTH, HEIGHT);
+
+        //draw background
+
+        g.drawImage(background, 0, 0, WIDTH, HEIGHT, null);
+
+        //draw the image of your objects below:
+        //DRAWING IMAGES ON SCREEN
+
+
+        //dispose the images each time(this allows for the illusion of movement).
+        g.dispose();
+
+        bufferStrategy.show();
+    }
+
+    //(4)***MOVE THINGS***
+    public void moveThings() {
+        //call the move() method code from your object class
+        //***CALLING move() FOR OBJECTS***
+        collisions();
+    }
+
+    public void collisions() {
+    }
+    //Pauses or sleeps the computer for the amount specified in milliseconds
+    public void pause ( int time){
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+
+        }
+    }
+
+
+    //Graphics setup method
+    private void setUpGraphics() {
+        frame = new JFrame("Game Land");   //Create the program window or frame.  Names it.
+
+        panel = (JPanel) frame.getContentPane();  //sets up a JPanel which is what goes in the frame
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));  //sizes the JPanel
+        panel.setLayout(null);   //set the layout
+
+        // creates a canvas which is a blank rectangular area of the screen onto which the application can draw
+        // and trap input events (Mouse and Keyboard events)
+        canvas = new Canvas();
+        canvas.setBounds(0, 0, WIDTH, HEIGHT);
+        canvas.setIgnoreRepaint(true);
+
+        panel.add(canvas);  // adds the canvas to the panel.
+
+        // frame operations
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //makes the frame close and exit nicely
+        frame.pack();  //adjusts the frame and its contents so the sizes are at their default or larger
+        frame.setResizable(false);   //makes it so the frame cannot be resized
+        frame.setVisible(true);      //IMPORTANT!!!  if the frame is not set to visible it will not appear on the screen!
+
+        // sets up things so the screen displays images nicely.
+        canvas.createBufferStrategy(2);
+        bufferStrategy = canvas.getBufferStrategy();
+        canvas.requestFocus();
+        System.out.println("DONE graphic setup");
+    }
+}
---
+++ src/Hero.java
@@ -0,0 +1,112 @@
+import java.awt.*;
+
+//***OBJECT CLASS***
+//objects are more complicated and encompass a lot of data
+//astro.xpos --> go to all astro data, retrieve xpos value
+//astro.rec --> both astro and rec are objects
+//entire apodosis is a boolean --> if(astro.rec.intersects(star.rec)) is a boolean
+public class Hero {
+    //declaration
+
+    public int xpos;            //the x position
+    public int ypos;            //the y position
+    public int dx;              //the speed of the hero in the x direction
+    public int dy;              //the speed of the hero in the y direction
+    public int width;           //image width
+    public int height;          //image height
+    public boolean isAlive;     //a boolean to denote if the hero is alive or not
+    public Rectangle rec;
+    //constructor
+
+    public Hero(int pXpos, int pYpos, int pDx, int pDy, int pWidth, int pHeight, boolean pIsAlive) {
+        xpos = pXpos;
+        ypos = pYpos;
+        dx=pDx;
+        dy=pDy;
+        width=pWidth;
+        height=pHeight;
+        isAlive=pIsAlive;
+        rec=new Rectangle(xpos,ypos,width,height);
+    }
+
+    public void printInfo() {
+        System.out.println("(x,y): ("+xpos+","+ypos+")");
+        System.out.println("dx: " + dx);
+        System.out.println("dy: " + dy);
+        System.out.println("width: " + width);
+        System.out.println("height: " + height);
+        System.out.println("isAlive: " + isAlive);
+    }
+
+    //***MOVE METHODS***
+    public void move() {
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+        rec = new Rectangle (xpos,ypos,width,height);
+    }
+    public void bounce() {
+        if (xpos>1000-width) {
+            dx = -dx;
+        }
+        if (xpos<0) {
+            dx = -dx;
+        }
+        if (ypos>700-height) {
+            dy = -dy;
+        }
+        if (ypos<0) {
+            dy = -dy;
+        }
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+        rec = new Rectangle (xpos,ypos,width,height);
+    }
+    public void changeDirection() {
+        dx=-dx;
+        dy=-dy;
+    }
+
+    public void stop() {
+        dx=0;
+        dy=0;
+    }
+
+    public void restart() {
+        dx=5;
+        dy=5;
+    }
+
+    public void randomMove() {
+        for(int i=0; i<3; i=i+1) {
+            dx=0;
+            dy=5*1;
+        }
+        dx=(int)(Math.random()*10);
+        dy=(int)(Math.random()*10);
+    }
+
+    public void teleport() {
+        xpos=(int)(Math.random()*1000);
+        ypos=(int)(Math.random()*700);
+        dx=(int)(Math.random()*10);
+        dy=(int)(Math.random()*10);
+    }
+
+    public void wrap() {
+        if (xpos>1000) {
+            xpos = 0;
+        }
+        if (xpos<0) {
+            xpos = 1000;
+        }
+        if (ypos>700) {
+            ypos = 0;
+        }
+        if (ypos<0) {
+            ypos = 700;
+        }
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+        rec = new Rectangle (xpos,ypos,width,height);
+    }
+}
